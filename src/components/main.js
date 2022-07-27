944f112dc75d615d2821a5a6239391c36c7fb6ce
Ahmed Mustafa <Ahmedzakho925@gmail.com>
2022-07-27 18:02:25 +0300

    finish the logic

--- src/components/main.js
+++ src/components/main.js
@@ -1,30 +1,63 @@
+import { useState } from "react";
 import classes from "./main.module.css";
 
 function Main() {
+  const [charCount, setCharCount] = useState(0);
+  const [wordCount, setWordCount] = useState(0);
+  const [sentenceCount, setSentenceCount] = useState(0);
+  const [paragraphCount, setParagraphCount] = useState(0);
+  const [readingTimeMin, setReadingTimeMin] = useState(0);
+  const [readingTimeHour, setReadingTimeHour] = useState(0);
+  const inputChangeHandler = (event) => {
+    const text = event.target.value;
+    const characterNumber = text.split("").length;
+    const wordsNumber = text.split(/[;:.\s]+/).length;
+    const sentenceNumber = text.split(".").length;
+    const paragraphsNumber = text.split(".\n").length;
+    const readingT = wordsNumber / 250;
+    const readingTHour = Math.trunc(readingT / 60);
+    const readingTMin = readingT % 60;
+    setWordCount(wordsNumber);
+    setCharCount(characterNumber);
+    setSentenceCount(sentenceNumber);
+    setParagraphCount(paragraphsNumber);
+    setReadingTimeHour(readingTHour);
+    setReadingTimeMin(readingTMin);
+    if (text === "") {
+      setWordCount(0);
+      setCharCount(0);
+      setSentenceCount(0);
+      setParagraphCount(0);
+      setReadingTimeHour(0);
+      setReadingTimeMin(0);
+    }
+  };
   return (
     <main className={classes.main}>
-      <textarea></textarea>
+      <textarea onChange={inputChangeHandler}></textarea>
       <div>
         <div>text details</div>
         <div>
           <span>words</span>
-          <span className="words">0</span>
+          <span className="words">{wordCount}</span>
         </div>
         <div>
           <span>characters</span>
-          <span className="characters">0</span>
+          <span className="characters">{charCount}</span>
         </div>
         <div>
           <span>sentences</span>
-          <span className="sentences">0</span>
+          <span className="sentences">{sentenceCount}</span>
         </div>
         <div>
           <span>paragraphs</span>
-          <span className="paragraphs">0</span>
+          <span className="paragraphs">{paragraphCount}</span>
         </div>
         <div>
           <span>reading time</span>
-          <span className="time">0</span>
+          <span className="time">
+            {readingTimeHour} H : {readingTimeMin.toFixed(0)} MIN
+          </span>
         </div>
       </div>
     </main>
